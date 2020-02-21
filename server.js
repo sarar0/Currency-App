35429562525253b633b99beac2170a2e818c90b5
Sara <srancati0@gmail.com>
2020-02-21 11:04:41 +0000

    Add dependencies

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
 
-// console.log that your server is up and running
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
--- src/components/Display.js
+++ src/components/Display.js
@@ -6,13 +6,31 @@ class Display extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: null
           }
     }
+
+    componentDidMount() {
+        this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response;
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      console.log(body)
+      return body;
+    };
 
     render() {
         return (
             <div>
                 <Form/>
+                {this.state.data}
             </div>
         )
     }

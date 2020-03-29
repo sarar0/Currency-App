85dae398c25e215c8a73a0238fd9278013e7e692
Sara <srancati0@gmail.com>
2020-03-29 13:49:20 +0100

    Add fetch

--- server.js
+++ server.js
@@ -2,28 +2,34 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-const Api = require('./services/apiCalls');
+const Api = require('./src/services/apiCalls');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// app.get('/express_backend', (req, res) => {
-//   res.send({})
+app.get('/express_backend', (req, res) => {
+  const conversion =  Api.apiCall()                   
 
+  console.log("conversion", conversion)    
 
+  res.send({
+      conversion
+   })
+})
 
 // Test routes
-app.get('/express_backend', (req, res) => {
-  res.send({ 
-      results : {
-        "GBP": {
-          "EUR": 1.09, 
-          "USD": 1.21
-        }, 
-        "EUR": {
-        }
-      }    
-     });
-});
+// app.get('/express_backend', (req, res) => {
+
+//   res.send({ 
+//       results : {
+//         "GBP": {
+//           "EUR": 1.09, 
+//           "USD": 1.21
+//         }, 
+//         "EUR": {
+//         }
+//       }    
+//      });
+// });
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -4,7 +4,22 @@ require('dotenv').config();
 var converter_key = process.env.CONVERTER_KEY;
 var mode   = process.env.NODE_ENV;
 
-async function apiCall(req, mode) {}
+function apiCall(req, mode) {
+    var currency_from = encodeURIComponent("USD");
+    var currency_to = encodeURIComponent("PHP");
+
+    var url = `https://free.currconv.com/api/v7/convert?q=${currency_from}_${currency_to}&compact=ultra&apiKey=${converter_key}`
+
+    var result = fetch(url) 
+        .then(data=>data.json())
+        .then(json=>json)
+        .catch(error => {
+        console.log(error)
+    });
+
+    return result
+};
+
 
 
 exports.apiCall = apiCall;
